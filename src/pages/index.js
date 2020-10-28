3dcca0c27ce5ccfbacad453dbeb77d3ff6818c4b
rohan-krishna <phonemg30993@gmail.com>
2020-10-29 00:34:30 +0530

    Logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import React from "react"
+import Logo from "../images/logo-1.png"
 
 export default function Home() {
-  return <div>
-    <h1 className="text-3xl">Hello world!</h1>
-  </div>
-}
+  return (
+    <div className="block w-full h-screen block bg-gray-100">
+		<div className="flex flex-col w-full px-3 py-3 items-center">
+			<img src={Logo} alt="" style={{ width: 128 }} />
+			<h1 className="text-3xl">Blue Hex Software</h1>
+		</div>
+    </div>
+    )
+  }
+  

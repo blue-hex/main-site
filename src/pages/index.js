a34704014342dfe15e99259b2d96585f1bc69b9b
Rohan Krishna <phonemg30993@gmail.com>
2020-11-16 01:35:45 +0530

    links added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,20 @@ import Logo from "../images/logo-1.png"
 
 export default function Home() {
 	return (
-		<div className="block w-full block bg-gray-300 py-3">
+		<div className="block w-full h-screen block py-3">
 			<div className="flex w-full px-3 py-3 items-center container mx-auto">
-				<img src={Logo} alt="" style={{ width: 128 }} className="m-0" />
+				<img src={Logo} alt="" style={{ width: 180 }} className="m-0" />
 				<span>
 					<h1 className="text-3xl">Blue Hex Software</h1>
 					<p>Enterprise Software Development & Business IT Consulting</p>
+					<ul className="flex space-x-3">
+						<li>
+							<a className="font-bold uppercase" href="#">Hello</a>
+						</li>
+						<li>
+							<a href="#">Hello</a>
+						</li>
+					</ul>
 				</span>
 			</div>
 		</div>

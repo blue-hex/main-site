9932e17deda27c3b53ef7e157caed3fef883900b
Rohan Krishna <phonemg30993@gmail.com>
2020-11-15 12:58:14 +0530

    Working on home page. Will add more sections.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,16 @@ import React from "react"
 import Logo from "../images/logo-1.png"
 
 export default function Home() {
-  return (
-    <div className="block w-full h-screen block bg-gray-100">
-		<div className="flex flex-col w-full px-3 py-3 items-center">
-			<img src={Logo} alt="" style={{ width: 128 }} />
-			<h1 className="text-3xl">Blue Hex Software</h1>
+	return (
+		<div className="block w-full block bg-gray-300 py-3">
+			<div className="flex w-full px-3 py-3 items-center container mx-auto">
+				<img src={Logo} alt="" style={{ width: 128 }} className="m-0" />
+				<span>
+					<h1 className="text-3xl">Blue Hex Software</h1>
+					<p>Enterprise Software Development & Business IT Consulting</p>
+				</span>
+			</div>
 		</div>
-    </div>
-    )
-  }
-  
+		)
+	}
+	

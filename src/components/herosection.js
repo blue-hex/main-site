e520d64a41723ec2f212cd6c9909b5d1f194db1c
Rohan Krishna <phonemg30993@gmail.com>
2020-11-17 23:42:50 +0530

    Hero section under construction.

--- src/components/herosection.js
+++ src/components/herosection.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Typed from 'typed.js';
 
 const HeroSection = (props) => {
+
+    const typedElement = useRef();
+
+    const options = {
+        strings: [
+            'you `transform` your business `digitally`.',
+            'you earn more smiles from your satisfied customers with our dedicated customer experience management software.',
+            'you build a great reputation and drive business growth with our efficient workflow and our experienced team.',
+        ],
+        typeSpeed: 40,
+        backSpeed: 40,
+        loop: true,
+    }
+
+    useEffect( () => {
+        console.log(typedElement)
+        new Typed(typedElement.current, options);
+    }, []) 
+
     return (
         <div className="container mx-auto">
-            <h3>We're Blue Hex</h3>
-            <p>
-                We're here to help ...
+            <h3 className="text-5xl">We're Blue Hex</h3>
+            <p className="w-1/2 ml-2">
+                We can help <span className="text-blue-700 font-base" ref={typedElement}></span>
             </p>
-            <ul>
-                <li>
-                    you transform your business digitally.
-                </li>
-                <li>
-                    you earn more smiles from your satisfied customers with our dedicated customer experience management software.
-                </li>
-                <li>
-                    you build a great reputation and drive business growth using our efficient workflow and industry-leading experienced team.
-                </li>
-            </ul>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
 	return (
 		<div className="block w-full h-screen block py-3">
 			{/* Header and Navigation */}
-			<div className="flex w-full px-3 py-3 items-center container mx-auto">
-				<img src={Logo} alt="" style={{ width: 180 }} className="m-0" />
+			<div className="flex w-full py-3 items-center container mx-auto">
+				<img src={Logo} alt="" style={{ width: 84.21 }} className="m-0 p-0" />
 				<span>
 					<h1 className="text-3xl">Blue Hex Software</h1>
 					<p>Enterprise Software Development & Business IT Consulting</p>
